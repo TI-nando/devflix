0ba7d10522acf50c6cc3289839ba9e9d15e21a49
Fernando Henrique <fernandoagro.654@gmail.com>
2025-09-17 20:52:52 -0300

    removido os comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header.jsx'; // AQUI
-import Footer from './components/Footer.jsx'; // AQUI
-import Home from './pages/Home.jsx'; // AQUI
-import MovieDetails from './pages/MovieDetails.jsx'; // AQUI
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Home from './pages/Home.jsx';
+import MovieDetails from './pages/MovieDetails.jsx';
 import './index.css';
 
 function App() {
