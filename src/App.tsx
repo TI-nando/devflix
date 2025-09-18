86e64adadf71bd3e3dcb24d0022edb41c6381f80
Fernando Henrique <fernandoagro.654@gmail.com>
2025-09-18 00:05:02 -0300

    adicionado as funcionalidades para as paginas  e restruturado o codigo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './pages/Home.jsx';
-import MovieDetails from './pages/MovieDetails.jsx';
-import './index.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import MovieDetails from "./pages/MovieDetails.jsx";
+import Movies from "./pages/Movies.jsx";
+import TVShows from "./pages/TVShows.jsx";
+import TVShowDetails from "./pages/TVShowDetails.jsx";
+import Favorites from "./pages/Favorites.jsx";
+import "./index.css";
 
 function App() {
   return (
@@ -14,6 +18,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/movie/:id" element={<MovieDetails />} />
+            <Route path="/tv/:id" element={<TVShowDetails />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/tv" element={<TVShows />} />
+            <Route path="/favorites" element={<Favorites />} />
           </Routes>
         </main>
         <Footer />
